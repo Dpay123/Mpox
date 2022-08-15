354b994fe06cc2ef47cbbd88f476264bdd374fde
Dominic Payer <payerdom@my.uri.edu>
2022-08-15 15:43:08 -0400

    task functions

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
+from distutils.log import Log
 from django.contrib import admin
 
+from .models import LogEntry, Task
+from .models import *
 # Register your models here.
+admin.site.register(LogEntry)
+admin.site.register(Task)
--- app/views.py
+++ app/views.py
@@ -8,12 +8,29 @@ class LogEntryForm(ModelForm):
         model = LogEntry
         fields = ['version', 'desc']
 
+class TaskEntryForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ['priority', 'desc']
+
 # Create your views here.
 def index(request):
     return render(request, "app/index.html")
 
 def todo(request):
-    return render(request, 'app/todo.html')
+    if request.method == 'GET':
+        tasks = Task.objects.all().order_by('priority').values()
+        form = TaskEntryForm()
+        context = {
+            "tasks": tasks,
+            "form": form
+        }
+        return render(request, 'app/todo.html', context)
+    else:
+        form = TaskEntryForm(request.POST)
+        if form.is_valid():
+            entry = form.save()
+            return redirect('todo')
 
 def log(request):
     if request.method == 'GET':
