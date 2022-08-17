d2ba9d37f94cadef4da331898c25a0eadcfdf2ff
Dominic Payer <payerdom@my.uri.edu>
2022-08-17 18:40:38 -0400

    totals

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from sqlite3 import IntegrityError
+from tkinter.tix import INTEGER
 from django.shortcuts import render, redirect
+from django.db.models import Sum
 from datetime import datetime
 
 from . import convert
@@ -8,19 +10,26 @@ from .forms import *
 def index(request):
     if request.method == 'GET':
         cases = CaseEntry.objects.all()
+        totalC = CaseEntry.objects.all().aggregate(Sum('numCases'))
+        totalD = CaseEntry.objects.all().aggregate(Sum('numDeaths'))
         context = {
             'cases': cases,
-            'form': DateFilter()
+            'form': DateFilter(),
+            'totalC': totalC['numCases__sum'],
+            'totalD': totalD['numDeaths__sum']
         }
         return render(request, "app/index.html", context)
     else:
         # dateInput variable stores format 'yyyy-mm-dd'
         dateInput = request.POST['date']
         cases = CaseEntry.objects.filter(date=dateInput)
+        totalC = CaseEntry.objects.filter(date=dateInput).aggregate(Sum('numCases'))
+        totalD = CaseEntry.objects.filter(date=dateInput).aggregate(Sum('numDeaths'))
         context = {
             'cases': cases,
             'form': DateFilter(request.POST),
-            'date': dateInput
+            'totalC': totalC['numCases__sum'],
+            'totalD': totalD['numDeaths__sum']
         }
         return render(request, "app/index.html", context)
 
